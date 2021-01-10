9fce189dcff04c45cdc222dfc1c3bf5547d7cd76
James Robertson <james@jamesrobertson.eu>
2021-01-10 00:33:51 +0000

    feature: An outline is now in sorted order by default.

--- lib/mindwords.rb
+++ lib/mindwords.rb
@@ -5,12 +5,14 @@
 require 'rexle'
 
 class MindWords
-
+  using ColouredText
+  
   def initialize(s, debug: false)
 
     @debug = debug
-
-    lines = s.strip.lines.map do |line|
+    @a = s.strip.lines
+    
+    lines = @a.map do |line|
 
       word = line.split(/ (?=#)/,2)
 
@@ -41,6 +43,8 @@ class MindWords
 
     end
 
+    @h = h
+    
     a = rexlize(h)
     doc = Rexle.new(['root', {}, '', *a])
 
@@ -64,12 +68,90 @@ class MindWords
     redundants.compact.each {|x| doc.element(x).delete }
     rm_duplicates(doc)
     
+    @xml = doc.root.xml pretty: true
     @outline = treeize doc.root
     
   end
+  
+  def search(keyword)
+    
+    a = @a.grep(/#{keyword}/i).map do |line|
+      
+      words = line.split
+      r = words.grep /#{keyword}/i
+      i = words.index r[0]
+      
+      [line, i]
+      
+    end
+    
+    return nil if a.empty?
+    #return a[0][0] if a.length < 2
 
-  def to_outline()
-    @outline
+    a2 = a.sort_by(&:last).map(&:first)
+    MindWords.new(a2.join)
+    
+  end
+  
+  def sort()
+    s = @a.sort.join
+    
+    def s.to_s()
+      self.lines.map do |x|
+        title, hashtags = x.split(/(?=#)/,2)
+        title + hashtags.chomp.brown
+      end.join("\n")
+    end    
+    
+    return s
+  end
+  
+  def sort!()
+    @a = sort().lines
+    self
+  end
+  
+  def tag_sort()
+    
+    h = @a.group_by  {|x| x[/#\w+/]}    
+    s = h.sort.map {|key, value| value.sort }.join
+    
+    def s.to_s()
+      self.lines.map do |x|
+        title, hashtags = x.split(/(?=#)/,2)
+        title + hashtags.chomp.brown
+      end.join("\n")
+    end
+    
+    return s
+    
+  end
+  
+  def tag_sort!()
+    @a = tag_sort().lines
+    self
+  end
+  
+  def to_h()
+    @h
+  end
+
+  def to_outline(sort: true)
+    sort ? a2tree(tree_sort(LineTree.new(@outline).to_a)) : @outline
+  end
+  
+  def to_s(colour: false)
+    
+    return @a.join unless colour
+    
+    @a.map do |x|
+        title, hashtags = x.split(/(?=#)/,2)
+        title + hashtags.chomp.brown
+    end.join("\n")
+  end
+  
+  def to_xml()
+    @xml
   end
 
   private
@@ -130,6 +212,26 @@ class MindWords
 
     lines.join
   end
+  
+  def tree_sort(a)
+
+    if a.first.is_a? Array then
+      a.sort_by(&:first).map {|x|  tree_sort(x) }
+    elsif a.any?
+      [a.first] + tree_sort(a[1..-1])
+    else
+      []
+    end
+  end
+
+  def a2tree(a, indent=0)
+
+    a.map do |row|
+      title, *remaining = row
+      children = remaining ? a2tree(remaining, indent+1) : ''
+      ('  ' * indent) + title + "\n" + children
+    end.join
+
+  end  
 
 end
-
