f4005a0e016572b789fa202e0bbf997f0831db12
James Robertson <james@jamesrobertson.me.uk>
2021-01-25 15:18:21 +0000

    feature: The lines attributes accessor can now be used to modify,delete, or add news lines.

--- lib/mindwords.rb
+++ lib/mindwords.rb
@@ -20,26 +20,134 @@ class MindWords
   using ColouredText
   using HashCopy
   
+  attr_accessor :lines
   
   def initialize(raws, parent: nil, debug: false)
 
-    @debug = debug
+    @parent, @debug = parent, debug
     
     s, _ = RXFHelper.read raws
-    @a = s.strip.gsub(/^\n/,'').lines
-    @a.shift if @a.first =~ /<\?mindwords\?>/
+    @lines = s.strip.gsub(/^\n/,'').lines
+    @lines.shift if @lines.first =~ /<\?mindwords\?>/
     
-    lines = @a.map do |line|
+  end
+  
+  def element(id)
+    
+    doc = Rexle.new(@xml)
+    e =  doc.root.element("//*[@id='#{id}']")
+    #e.attributes[:breadcrumb].to_s if e
+    
+  end
 
-      word = line.split(/ (?=#)/,2)
-
+  
+  def search(keyword)
+    
+    a = @lines.grep(/#{keyword}/i).map do |line|
+      
+      puts 'line: ' + line.inspect if @debug
+      
+      words = line.split
+      r = words.grep /#{keyword}/i
+      i = words.index r[0]
+      
+      [line, i]
+      
     end
     
+    return nil if a.empty?
+    #return a[0][0] if a.length < 2
+
+    a2 = a.sort_by(&:last).map(&:first)
+    puts 'a2: ' + a2.inspect if @debug
+    MindWords.new(a2.join, parent: keyword, debug: @debug)
+    
+  end
+  
+  def sort()
+    s = @lines.sort.join
+    
+    def s.to_s()
+      self.lines.map do |x|
+        title, hashtags = x.split(/(?=#)/,2)
+        title + hashtags.chomp.brown
+      end.join("\n")
+    end    
+    
+    return s
+  end
+  
+  def sort!()
+    @lines = sort().lines
+    self
+  end
+  
+  def tag_sort()
+    
+    h = @lines.group_by  {|x| x[/#\w+/]}    
+    s = h.sort.map {|key, value| value.sort }.join
+    
+    def s.to_s()
+      self.lines.map do |x|
+        title, hashtags = x.split(/(?=#)/,2)
+        title + hashtags.chomp.brown
+      end.join("\n")
+    end
+    
+    return s
+    
+  end
+  
+  def tag_sort!()
+    @lines = tag_sort().lines
+    self
+  end
+  
+  def to_h()
+    @h
+  end
+  
+  def to_hashtags()
+    @hashtags
+  end
+
+  def to_outline(sort: true)
+    build()
+    sort ? a2tree(tree_sort(LineTree.new(@outline).to_a)) : @outline
+  end
+  
+  def to_s(colour: false)
+    
+    header = "<?mindwords?>\n\n"
+    return header + @lines.join unless colour
+    
+    body = @lines.map do |x|
+        title, hashtags = x.split(/(?=#)/,2)
+        title + hashtags.chomp.brown
+    end.join("\n")
+    
+    header + body
+    
+  end
+  
+  def to_words()
+    to_outline.lines.map {|x| x[/\w[\w ]+/] }.uniq
+  end
+  
+  def to_xml()
+    @xml
+  end
+
+  private
+
+  def build()
+    
     h = {}
-    @hashtags = {}
 
-    lines.each do |title, rawtags|
+    @lines.each do |line|
 
+      title, rawtags = line.split(/ (?=#)/,2)
+      
       rawtags.scan(/#(\w+)/).flatten(1).each do |rawtag|
         tag = rawtag.gsub(/ +/, '_')
         h[tag] ||= []
@@ -89,8 +197,8 @@ class MindWords
     redundants.compact.each {|x| doc.element(x).delete }
  
     
-    node = if parent then
-      found = doc.root.element('//' + parent)
+    node = if @parent then
+      found = doc.root.element('//' + @parent)
       found ? found : doc.root
     else
       doc.root
@@ -106,7 +214,7 @@ class MindWords
           e.parent.attributes[:breadcrumb].to_s  + ' / ' : ''
       e.attributes[:breadcrumb] = s +  e.value.strip
       
-      r = @a.grep(/^#{e.attributes[:title]} #/i)
+      r = @lines.grep(/^#{e.attributes[:title]} #/i)
       next unless r.any?
       e.attributes[:hashtags] = r[0].scan(/(?<=#)\w+/).join(' ')
 
@@ -114,115 +222,9 @@ class MindWords
     end
     
     @xml = node.xml pretty: true
-    
-  end
-  
-  def element(id)
-    
-    doc = Rexle.new(@xml)
-    e =  doc.root.element("//*[@id='#{id}']")
-    #e.attributes[:breadcrumb].to_s if e
-    
-  end  
-  
-  def search(keyword)
-    
-    a = @a.grep(/#{keyword}/i).map do |line|
-      
-      puts 'line: ' + line.inspect if @debug
-      
-      words = line.split
-      r = words.grep /#{keyword}/i
-      i = words.index r[0]
-      
-      [line, i]
-      
-    end
-    
-    return nil if a.empty?
-    #return a[0][0] if a.length < 2
 
-    a2 = a.sort_by(&:last).map(&:first)
-    puts 'a2: ' + a2.inspect if @debug
-    MindWords.new(a2.join, parent: keyword, debug: @debug)
-    
   end
   
-  def sort()
-    s = @a.sort.join
-    
-    def s.to_s()
-      self.lines.map do |x|
-        title, hashtags = x.split(/(?=#)/,2)
-        title + hashtags.chomp.brown
-      end.join("\n")
-    end    
-    
-    return s
-  end
-  
-  def sort!()
-    @a = sort().lines
-    self
-  end
-  
-  def tag_sort()
-    
-    h = @a.group_by  {|x| x[/#\w+/]}    
-    s = h.sort.map {|key, value| value.sort }.join
-    
-    def s.to_s()
-      self.lines.map do |x|
-        title, hashtags = x.split(/(?=#)/,2)
-        title + hashtags.chomp.brown
-      end.join("\n")
-    end
-    
-    return s
-    
-  end
-  
-  def tag_sort!()
-    @a = tag_sort().lines
-    self
-  end
-  
-  def to_h()
-    @h
-  end
-  
-  def to_hashtags()
-    @hashtags
-  end
-
-  def to_outline(sort: true)
-    sort ? a2tree(tree_sort(LineTree.new(@outline).to_a)) : @outline
-  end
-  
-  def to_s(colour: false)
-    
-    header = "<?mindwords?>\n\n"
-    return header + @a.join unless colour
-    
-    body = @a.map do |x|
-        title, hashtags = x.split(/(?=#)/,2)
-        title + hashtags.chomp.brown
-    end.join("\n")
-    
-    header + body
-    
-  end
-  
-  def to_words()
-    to_outline.lines.map {|x| x[/\w[\w ]+/] }.uniq
-  end
-  
-  def to_xml()
-    @xml
-  end
-
-  private
-
   def rexlize(a)
     
     a.map do |x|
--- mindwords.gemspec
+++ mindwords.gemspec
@@ -1,6 +1,6 @@
 Gem::Specification.new do |s|
   s.name = 'mindwords'
-  s.version = '0.5.0'
+  s.version = '0.5.1'
   s.summary = 'Helps get what\'s in your mind into a structure using ' + 
       'words and hashtags.'
   s.authors = ['James Robertson']
